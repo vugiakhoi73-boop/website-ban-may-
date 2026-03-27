c0fb037953210584c54770b6e227402ade2b2be4
vugiakhoi73-boop <vugiakhoi73@gmail.com>
2026-03-27 22:03:59 +0700

    Update database configuration settings

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "localhost";
-$configDB["database"]	= "computer_store";
+$configDB["host"] 		= "192.168.174.154";
+$configDB["database"]	= "websitebanmaytinh";
 $configDB["username"] 	= "root";
-$configDB["password"] 	= "";
-define("HOST", "localhost");
-define("DB_NAME", "computer_store");
+$configDB["password"] 	= "1234";
+define("HOST", "192.168.174.154");
+define("DB_NAME", "websitebanmaytinh");
 define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_PASS", "1234");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
 define("BASE_URL", "http://".$_SERVER['SERVER_NAME']);//dia chi website
