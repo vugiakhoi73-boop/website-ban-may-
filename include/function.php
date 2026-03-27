f0ec82c90fdef9d46a2adcd3b5671dfae14e9b64
vugiakhoi73-boop <vugiakhoi73@gmail.com>
2026-03-27 22:23:05 +0700

    Update function.php

--- include/function.php
+++ include/function.php
@@ -1,16 +1,32 @@
 <?php
+// Hàm tự động nạp Class
 function loadClass($c) {
-    // Sử dụng đường dẫn tương đối để an toàn hơn
     $file = "classes/" . $c . ".php";
     if (file_exists($file)) {
         include_once $file;
     } else {
-        // Dự phòng nếu dùng hằng số ROOT
         $file_alt = __DIR__ . "/../classes/" . $c . ".php";
         if (file_exists($file_alt)) {
             include_once $file_alt;
         }
     }
 }
-?>
 
+// Hàm lấy tham số điều hướng (Sửa lỗi dòng 2 mode.php)
+function getIndex() {
+    $r = "home"; 
+    if (isset($_GET['mod'])) {
+        $r = $_GET['mod'];
+    }
+    return $r;
+}
+
+// Hàm lấy tham số hành động (Nếu web bạn có dùng)
+function getP(($n) {
+    $r = "";
+    if (isset($_GET[$n])) {
+        $r = $_GET[$n];
+    }
+    return $r;
+}
+?>
